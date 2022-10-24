c24f7bb89123757482c1e268795682a6569c977b
Arthur Peixoto <98115467+Arthurpeixoto25@users.noreply.github.com>
2022-10-24 07:59:45 -0300

    Finalizando o Imask

--- explorer-lab-01-main/src/main.js
+++ explorer-lab-01-main/src/main.js
@@ -19,9 +19,59 @@ const colors = {
 
 setCardType("mastercard")
 
-const securitycode = document.querySelector('#security-code')
+const securitycode = document.querySelector("#security-code")
 const secutityCodePattern = {
     mask: "0000"
 }
 
 const secutityCodeMask = Imask(securitycode, secutityCodePattern)
+
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+    mask: "MM{/}YY", 
+    blocks:{
+        MM:{
+            mask:Imask.MaskedRange,
+            from: 1,
+            to: 12
+        },
+        YY:{
+            mask:Imask.MaskedRange,
+            from: 22,
+            to: 32
+        }
+    }
+
+}
+
+const securityDateMask = Imask(expirationDate, expirationDatePattern)
+
+
+const CardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+    mask:[
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /^4\d{0,15}/,
+            cardType: "visa"
+        },
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+            cardType: "mastercard"
+        },
+        {
+            mask: "0000 0000 0000 0000",
+            cardType: "default"
+        }
+    ],
+    dispatch: function(appended, dynamicMasked){
+        const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+        const foundMask = dynamicMasked.compiledMasks.find(function(item){
+         return number.match(item.regex)   
+        })
+        return foundMask
+    }
+}
+
+const CardNumberMask = Imask(CardNumber, cardNumberPattern)
